0e33d679b37672fcd060eb9f46b02a2452b0e49e
minye065 <minye065@fusdk12.net>
2026-08-20 18:48:09 -0700

    bug fix

--- src/win32.c
+++ src/win32.c
@@ -82,7 +82,7 @@ Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
             ShowWindow(BackButton, SW_SHOW);
             InvalidateRect(Window, NULL, TRUE);
             input = GetWindowTextA(InputBox, input, 100000000);
-            noteCount = input;
+            noteCount = input.;
             noteStorage = malloc(noteCount * sizeof(note))
 
         } break;
